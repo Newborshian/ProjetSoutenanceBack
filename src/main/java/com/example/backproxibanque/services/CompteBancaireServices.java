5672cff5a26bedc56743893773a88f7ac1107f52
Annabelle M <wouabelle@gmail.com>
2023-09-14 10:56:08 +0200

    Ajout de getAllCB()dans CompteBancaireController

--- src/main/java/com/example/backproxibanque/services/CompteBancaireServices.java
+++ src/main/java/com/example/backproxibanque/services/CompteBancaireServices.java
@@ -1,13 +1,14 @@
 package com.example.backproxibanque.services;
 
 import com.example.backproxibanque.dtos.ClientDto;
+import com.example.backproxibanque.dtos.CompteBancaireDto;
 import com.example.backproxibanque.entities.CompteCourant;
 import com.example.backproxibanque.entities.CompteEpargne;
 
 import java.util.List;
 
 public interface CompteBancaireServices {
-    CompteBancaireDto CompteCourantToDto(CompteCourant compteCourant);
-    CompteBancaireDto CompteEpargneToDto(CompteEpargne compteCourant);
+    CompteBancaireDto compteCourantToDto(CompteCourant compteCourant);
+    CompteBancaireDto compteEpargneToDto(CompteEpargne compteCourant);
     List<CompteBancaireDto> getAllCompteBancaire();
 }
--- src/main/java/com/example/backproxibanque/services/imp/CompteBancaireServicesImp.java
+++ src/main/java/com/example/backproxibanque/services/imp/CompteBancaireServicesImp.java
@@ -1,25 +1,69 @@
 package com.example.backproxibanque.services.imp;
 
+import com.example.backproxibanque.dtos.ClientDto;
 import com.example.backproxibanque.dtos.CompteBancaireDto;
+import com.example.backproxibanque.entities.Client;
 import com.example.backproxibanque.entities.CompteCourant;
 import com.example.backproxibanque.entities.CompteEpargne;
+import com.example.backproxibanque.enums.TypeDeCompte;
+import com.example.backproxibanque.repositories.CompteCourantRepository;
+import com.example.backproxibanque.repositories.CompteEpargneRepository;
 import com.example.backproxibanque.services.CompteBancaireServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class CompteBancaireServicesImp implements CompteBancaireServices {
+
+    @Autowired
+    private CompteCourantRepository compteCourantRepository;
+
+    @Autowired
+    private CompteEpargneRepository compteEpargneRepository;
     @Override
-    public CompteBancaireDto CompteCourantToDto(CompteCourant compteCourant) {
-        return null;
+    public CompteBancaireDto compteCourantToDto(CompteCourant compteCourant) {
+        CompteBancaireDto compteBancaireDto = new CompteBancaireDto();
+        compteBancaireDto.setId(compteCourant.getId());
+        compteBancaireDto.setNumerodecompte(compteCourant.getNumeroDeCompte());
+        compteBancaireDto.setTypeDeCompte(TypeDeCompte.COURANT.toString());
+        compteBancaireDto.setSolde(compteCourant.getSolde());
+        compteBancaireDto.setOverDraft(compteCourant.getOverdraft());
+        compteBancaireDto.setNameClient(compteCourant.getClient().getLastname() + " " + compteCourant.getClient().getFirstname());
+        return compteBancaireDto;
     }
 
     @Override
-    public CompteBancaireDto CompteEpargneToDto(CompteEpargne compteCourant) {
-        return null;
+    public CompteBancaireDto compteEpargneToDto(CompteEpargne compteEpargne) {
+        CompteBancaireDto compteBancaireDto = new CompteBancaireDto();
+        compteBancaireDto.setId(compteEpargne.getId());
+        compteBancaireDto.setNumerodecompte(compteEpargne.getNumeroDeCompte());
+        compteBancaireDto.setTypeDeCompte(TypeDeCompte.COURANT.toString());
+        compteBancaireDto.setSolde(compteEpargne.getSolde());
+        compteBancaireDto.setOverDraft(compteEpargne.getTauxInteret());
+        compteBancaireDto.setNameClient(compteEpargne.getClient().getLastname() + " " + compteEpargne.getClient().getFirstname());
+        return compteBancaireDto;
     }
 
     @Override
     public List<CompteBancaireDto> getAllCompteBancaire() {
-        return null;
+        List<CompteBancaireDto> compteBancaireDtoList = new ArrayList<>();
+        List<CompteCourant> compteCourantList = compteCourantRepository.findAll();
+        List<CompteEpargne> compteEpargneList = compteEpargneRepository.findAll();
+
+        if (!compteEpargneList.isEmpty() || !compteCourantList.isEmpty()){
+            if (!compteCourantList.isEmpty()){
+            for(CompteCourant  compteCourant : compteCourantList ){
+                compteBancaireDtoList.add(compteCourantToDto(compteCourant)) ;
+            }}
+            if (!compteEpargneList.isEmpty()) {
+                for(CompteEpargne  compteEpargne : compteEpargneList ){
+                    compteBancaireDtoList.add(compteEpargneToDto(compteEpargne)) ;
+            }}
+            return compteBancaireDtoList;
+        }
+        else throw new RuntimeException("Veuillez affilier des comptes bancaires à ce client");
     }
-}
+    }
+
